02179a0678df32e8c11c7f0c49ad5b7104b754af
Svetlana Zhuravleva <karpenter.xo@gmail.com>
2024-04-09 17:48:56 +0200

    update hw 09.04

--- src/main/java/lessons/lesson_09_04/Task.java
+++ src/main/java/lessons/lesson_09_04/Task.java
@@ -1,5 +1,6 @@
 package lessons.lesson_09_04;
 
+
 public class Task {
     public static void main(String[] args) {
         int[][] array = {{2, 3,	5, 6, 9},
@@ -10,7 +11,12 @@ public class Task {
 
         System.out.println(isNumInclude(array, 21));
     }
-
+    /**
+     *
+     * @param matrix матрица в которой все числа отсортированы справа налево и сверху вниз
+     * @param number число которое ищем
+     * @return true - если число есть матрице
+     */
     private static boolean isNumInclude(int[][] matrix, int number) {
         for (int[] ints : matrix) {
             for (int j = ints.length - 1; j > 0; j--) {
