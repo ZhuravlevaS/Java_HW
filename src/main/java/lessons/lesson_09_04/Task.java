3b4e20f22c607524a1458471058caaf58326e356
Svetlana Zhuravleva <karpenter.xo@gmail.com>
2024-04-09 17:47:27 +0200

    add hw 09.04

---
+++ src/main/java/lessons/lesson_09_04/Task.java
@@ -0,0 +1,28 @@
+package lessons.lesson_09_04;
+
+public class Task {
+    public static void main(String[] args) {
+        int[][] array = {{2, 3,	5, 6, 9},
+                {4,	7,	8,	11,	13},
+                {14, 19, 25, 26, 27},
+                {15, 21, 28, 33, 36},
+                {24, 29, 44, 55, 66}};
+
+        System.out.println(isNumInclude(array, 21));
+    }
+
+    private static boolean isNumInclude(int[][] matrix, int number) {
+        for (int[] ints : matrix) {
+            for (int j = ints.length - 1; j > 0; j--) {
+                if (ints[j] < number) {
+                    break;
+                }
+
+                if (ints[j] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
