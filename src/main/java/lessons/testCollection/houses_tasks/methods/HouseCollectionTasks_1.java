3793d776322eb2a2479029e7e125aa061f34e071
Svetlana Zhuravleva <karpenter.xo@gmail.com>
2024-05-09 19:43:55 +0200

    made HouseCollectionTasks_1

--- src/main/java/lessons/testCollection/houses_tasks/methods/HouseCollectionTasks_1.java
+++ src/main/java/lessons/testCollection/houses_tasks/methods/HouseCollectionTasks_1.java
@@ -1,9 +1,6 @@
 package lessons.testCollection.houses_tasks.methods;
 
-import lessons.testCollection.houses_tasks.Color;
-import lessons.testCollection.houses_tasks.Flat;
-import lessons.testCollection.houses_tasks.House;
-import lessons.testCollection.houses_tasks.HouseGenerator;
+import lessons.testCollection.houses_tasks.*;
 
 import java.util.*;
 
@@ -12,14 +9,27 @@ public class HouseCollectionTasks_1 {
     // Задание 1: Вернуть список всех квартир во всех домах (List)
     public static List<Flat> getAllFlats(List<House> houses) {
         List<Flat> allFlats = new ArrayList<>();
-
+        for (House house: houses){
+            allFlats.addAll(house.getFlats());
+        }
         return allFlats;
     }
 
     // Задание 2: Вернуть множество всех уникальных цветов, используемых в комнатах квартир (Set)
     public static Set<Color> getUniqueRoomColors(List<House> houses) {
         Set<Color> colors = new HashSet<>();
+        int colorsQuantity = Color.values().length;
 
+        for (House house: houses) {
+            if(colorsQuantity == colors.size()) break;
+            for (Flat flat: house.getFlats()){
+                if(colorsQuantity == colors.size()) break;
+                for (Room room: flat.getRoomList()){
+                    if(colorsQuantity == colors.size()) break;
+                    colors.add(room.getColor());
+                }
+            }
+        }
         return colors;
     }
 
@@ -28,7 +38,10 @@ public class HouseCollectionTasks_1 {
     // номера этажей, а значениями - количество квартир на этом этаже (Map)
     public static Map<Integer, Integer> getFlatsCountByFloor(House house) {
         Map<Integer, Integer> floorMap = new HashMap<>();
-
+        System.out.println(house);
+            for (Flat flat: house.getFlats()){
+                floorMap.put(flat.getFloor(), floorMap.getOrDefault(flat.getFloor(),0) + 1);
+            }
         return floorMap;
     }
 
@@ -43,16 +56,22 @@ public class HouseCollectionTasks_1 {
 
     // Задание 5: Используя стек, вернуть список всех квартир
     // в обратном порядке от последнего дома к первому (Stack)
-    public static List<Flat> getFlatsInReverseOrder(House houses) {
+    public static List<Flat> getFlatsInReverseOrder(House house) {
         Stack<Flat> stack = new Stack<>();
         List<Flat> reversedFlats = new ArrayList<>();
 
+        for (Flat flat: house.getFlats()){
+            stack.push(flat);
+        }
 
+        for (int i = 0; i < stack.size(); i++) {
+            reversedFlats.add(stack.pop());
+        }
         return reversedFlats;
     }
     
     public static void main(String[] args) {
-        List<House> houses = HouseGenerator.generateHouses(777);
+        List<House> houses = HouseGenerator.generateHouses(2);
 
         System.out.println("Все квартиры: " + getAllFlats(houses).size());
         System.out.println("Уникальные цвета комнат: " + getUniqueRoomColors(houses).size());
