570097d3211931e186ca5e54249e992e344062af
Svetlana Zhuravleva <karpenter.xo@gmail.com>
2024-06-05 19:19:48 +0200

    binary search

--- src/main/java/algorithms/homeworks/hw_05_30/BinarySearch.java
+++ src/main/java/algorithms/homeworks/hw_05_30/BinarySearch.java
@@ -5,26 +5,28 @@ import java.util.List;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        List<Integer> sortedList = List.of(1, 2, 3, 4, 5, 6, 7 ,7 , 8, 8, 8, 8, 8, 8, 9, 10, 11);
+        List<Integer> sortedList = List.of(1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 8, 8, 8, 8, 9, 10, 11);
 
-        System.out.println(search(8, sortedList));
+        System.out.println(search(6, sortedList, 0, sortedList.size() - 1));
     }
 
-    private static int search(int searchedElem, List<Integer> sortedList) {
-        if(sortedList.isEmpty()) {
+    private static int search(int searchedElem, List<Integer> sortedList, int startIdx, int endIdx) {
+        if (startIdx > endIdx) {
             return -1;
         }
 
-        int middleIndex = sortedList.size()/2;
+        int middleIndex = startIdx + (endIdx - startIdx) / 2;
+
 
         if (sortedList.get(middleIndex) == searchedElem) {
+            System.out.println("Index: " + middleIndex);
             return searchedElem;
         }
 
         if (searchedElem > sortedList.get(middleIndex)) {
-           return search(searchedElem,sortedList.subList(middleIndex + 1, sortedList.size()));
+            return search(searchedElem, sortedList, middleIndex + 1, endIdx);
         } else {
-           return search(searchedElem,sortedList.subList(0, middleIndex));
+            return search(searchedElem, sortedList, startIdx, middleIndex - 1);
         }
 
     }
